114775bb080cbf8685a73817113d78a89fb5a843
Paul Schifferer <paul@schifferers.net>
2020-05-19 15:17:53 -0700

    Add some debug logging.

--- application/controllers/actions/add.py
+++ application/controllers/actions/add.py
@@ -14,6 +14,7 @@ def execute(team_id:str, user_id:str, args:list) -> list:
     current_app.logger.debug("team_id: %s, user_id: %s, args: %s", team_id, user_id, args)
 
     updated_args = [args[0], model_constants.ADJUST_OP_ADD, args[1]]
+    current_app.logger.debug("updated_args: %s", updated_args)
 
     return adjust.execute(team_id, user_id, updated_args)
 
--- application/controllers/actions/inc.py
+++ application/controllers/actions/inc.py
@@ -14,5 +14,6 @@ def execute(team_id:str, user_id:str, args:list) -> list:
     current_app.logger.debug("team_id: %s, user_id: %s, args: %s", team_id, user_id, args)
 
     updated_args = [args[0], model_constants.ADJUST_OP_ADD, args[1]]
+    current_app.logger.debug("updated_args: %s", updated_args)
 
     return adjust.execute(team_id, user_id, updated_args)
