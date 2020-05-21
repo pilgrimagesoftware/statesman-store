a233cb088f1ee4f98d95b2b42774bdfbe80d7b68
Paul Schifferer <paul@schifferers.net>
2020-05-21 10:41:08 -0700

    Clean up docker linux script.

--- scripts/run-docker-linux.sh
+++ scripts/run-docker-linux.sh
@@ -4,16 +4,16 @@ set -x
 set -e
 set -o pipefail
 
+# sudo docker network create statesman
 sudo docker run \
     -d \
-    --name manager \
-    --hostname manager \
+    --name statesman \
+    --hostname statesman \
     -v $(pwd):/app \
-    -v /opt/minecraft/scripts:/scripts \
-    -v /opt/minecraft/servers:/servers \
-    -v /opt/minecraft/supervisor:/opt/minecraft/supervisor \
-    --net host \
     --workdir /app \
+    -p 8899:8899 \
+    --link pg-statesman \
+    --link redis-statesman \
     --restart unless-stopped \
     python:3.8 \
     /app/run.sh
