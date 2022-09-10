50c226bba35355e20ed110aca3af1a8f27849a18
Paul Schifferer <paul@schifferers.net>
2022-09-10 07:50:25 -0700

    Fix config path

--- src/statesman/main.py
+++ src/statesman/main.py
@@ -38,7 +38,7 @@ def create_app(app_name=constants.APPLICATION_NAME):
     })
 
     app = Flask(app_name)
-    app.config.from_object("application.config.BaseConfig")
+    app.config.from_object("statesman.config.BaseConfig")
     # env = DotEnv(app)
     # cache.init_app(app)
 
