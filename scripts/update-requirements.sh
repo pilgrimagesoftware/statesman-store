5999dd4cc2be4f5fb18271f3b0a11223d8a5ad66
Paul Schifferer <paul@schifferers.net>
2022-09-20 10:46:42 -0700

    Error trapping

--- scripts/update-requirements.sh
+++ scripts/update-requirements.sh
@@ -7,12 +7,14 @@ scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 pushd ${scriptdir}/..
 
-which pyenv
+set +e
+which pyenv || true
 if [ $? -eq 0 ]; then
     compile=$(pyenv which pip-compile)
 else
     compile=$(which pip-compile)
 fi
+set -e
 
 for r in app docs tests dev deploy; do
     echo ""
