eca712a25f680a95265bb1ab14cc5213497b6b2f
Paul Schifferer <paul@schifferers.net>
2022-09-20 11:09:37 -0700

    Forget it

--- scripts/update-requirements.sh
+++ scripts/update-requirements.sh
@@ -7,20 +7,11 @@ scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 pushd ${scriptdir}/..
 
-set +e
-which pyenv || true
-if [ $? -eq 0 ]; then
-    compile=$(pyenv which pip-compile)
-else
-    compile=pip-compile
-fi
-set -e
-
 for r in app docs tests dev deploy; do
     echo ""
     echo "----------------------------"
     echo -e "Requirement: \033[1m${r}\033[0m"
-    ${compile} -r -U requirements/${r}.in
+    pip-compile -r -U requirements/${r}.in
 done
 
 popd
