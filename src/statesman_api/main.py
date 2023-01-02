f780c5b7d8e819acb6c9fc246388c285b7600b4c
Paul Schifferer <paul@schifferers.net>
2023-01-02 10:02:49 -0800

    Remove commented code

--- src/statesman_api/main.py
+++ src/statesman_api/main.py
@@ -43,34 +43,14 @@ def create_app(app_name=constants.APPLICATION_NAME):
     # env = DotEnv(app)
     # cache.init_app(app)
 
-    session = Session(app)
+    app.session = Session(app)
 
-    # cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
-    wsgi = SentryWsgiMiddleware(app)
+    app.sentry = SentryWsgiMiddleware(app)
 
     app.executor = Executor(app)
 
-    # from application.blueprints.main.home import blueprint as home_blueprint
-    # app.register_blueprint(home_blueprint, url_prefix="/")
-
     from statesman_api.blueprints.api import blueprint as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix="/api/v1")
-
-    # from application.blueprints.apps import blueprint as apps_blueprint
-    # app.register_blueprint(apps_blueprint, url_prefix="/apps")
-
-    # from application.blueprints.account import blueprint as account_blueprint
-    # app.register_blueprint(account_blueprint, url_prefix="/account")
-
-    # from application.blueprints.auth import blueprint as auth_blueprint
-    # app.register_blueprint(auth_blueprint, url_prefix="/auth")
-
-    # from application.blueprints.main.health import blueprint as health_blueprint
-    # app.register_blueprint(health_blueprint, url_prefix="/health")
-
-    # from application.blueprints.billing import blueprint as billing_blueprint
-    # app.register_blueprint(billing_blueprint, url_prefix="/billing")
 
     from statesman_api.blueprints.health import blueprint as health_blueprint
     app.register_blueprint(health_blueprint, url_prefix="/health")
@@ -80,17 +60,6 @@ def create_app(app_name=constants.APPLICATION_NAME):
     db.init_app(app)
     # migrate = Migrate(app, db)
 
-    # vue = Vue(app)
-
-    # app.wsgi_app = SassMiddleware(app.wsgi_app, {
-    #     'application': ('static/sass', 'static/css', '/static/css')
-    # })
-    # scss = Scss(app, static_dir='static', asset_dir='assets')
-
-    # load_servers(app)
-
     print(app.url_map)
-
-    # app.debug = True
 
     return app
