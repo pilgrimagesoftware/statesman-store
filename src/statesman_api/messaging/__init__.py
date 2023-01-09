24111628ad3910bc51f80ef86299415b7b4e4ba6
Paul Schifferer <paul@schifferers.net>
2023-01-08 20:10:08 -0800

    Handle responding

--- src/statesman_api/messaging/__init__.py
+++ src/statesman_api/messaging/__init__.py
@@ -3,7 +3,7 @@ __author__ = "Paul Schifferer <paul@schifferers.net>"
 - Messaging
 """
 
-import logging, os
+import logging, os, json, time
 import pika
 from statesman_api import constants
 from threading import Thread
@@ -16,6 +16,25 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
             pika.PlainCredentials(os.environ[constants.RABBITMQ_USER], os.environ[constants.RABBITMQ_PASSWORD]),
         ))
 channel = connection.channel()
+
+
+def send_amqp_response(msg, response_data:dict):
+    """_summary_
+
+    Args:
+        msg (_type_): _description_
+    """
+    logging.debug("msg: %s", msg)
+
+    body_data = {
+        "sender": os.environ[constants.POD],
+        "timestamp": time.time(),
+        "response_data": response_data,
+        "answer": msg,
+    }
+    body = json.dumps(body_data)
+    logging.debug("body: %s", body)
+    channel.basic_publish(exchange=os.environ[constants.RABBITMQ_EXCHANGE], routing_key=response_data['queue'], body=body)
 
 
 class MessageConsumer(Thread):
@@ -35,6 +54,25 @@ class MessageConsumer(Thread):
             body (_type_): _description_
         """
         logging.info("ch: %s, method: %s, properties: %s, body: %s", ch, method, properties, body)
+
+        # extract info
+        msg = json.loads(body)
+        logging.debug("msg: %s", msg)
+        sender = msg['sender']
+        timestamp = msg['timestamp']
+        response_data = msg['response_data']
+        logging.debug("response_data: %s", response_data)
+        user = msg['user']
+        logging.debug("user: %s", user)
+        command = msg['data']['command']
+        logging.debug("command: %s", command)
+
+        # TODO: process the command
+        body_data = {}
+
+        # send reponse
+        send_amqp_response(body_data, response_data)
+
 
     def run(self):
         logging.info("Consumer thread started.")
